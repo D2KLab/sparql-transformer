ba3f2c6cd247f3d0d0b4ea607d152a281591daba
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2019-03-12 13:41:08 +0100

    playground

---
+++ docs/script.js
@@ -0,0 +1,110 @@
+import sparqlTransformer from 'sparql-transformer@2.0.3';
+import sparqlClient from 'sparql-transformer@2.0.3/src/sparql-client.mjs';
+import AsyncComputed from 'vue-async-computed';
+
+const jq = {
+  proto: {
+    id: '?id',
+    name: '$rdfs:label$required',
+    image: '$foaf:depiction$required',
+  },
+  $where: [
+    '?id a dbo:City',
+    '?id dbo:country dbr:Italy',
+  ],
+  $limit: 100,
+};
+
+const application = new Vue({
+  el: '#app',
+  data: {
+    input: JSON.stringify(jq, null, 2),
+    endpoint: 'http://dbpedia.org/sparql',
+    loading: false,
+    sparql: '',
+    frogJson: null,
+    princeJson: null,
+    jsonError: null,
+  },
+  created() {
+    this.computeSparql();
+  },
+  methods: {
+    computeSparql() {
+      const app = this;
+
+      let jsonQuery;
+      try {
+        jsonQuery = JSON.parse(this.input);
+      } catch (e) {
+        return;
+      }
+      sparqlTransformer(jsonQuery, {
+        endpoint: this.endpoint,
+        sparqlFunction(sparql) {
+          app.sparql = sparql;
+          return Promise.reject();
+        },
+      }).catch(() => {});
+    },
+    execute() {
+      const app = this;
+      app.frogJson = null;
+      app.princeJson = null;
+      app.jsonError = null;
+
+      let jsonQuery;
+      try {
+        jsonQuery = JSON.parse(this.input);
+      } catch (e) {
+        app.jsonError = e;
+        return;
+      }
+      this.loading = true;
+      sparqlTransformer(jsonQuery, {
+        sparqlFunction(sparql) {
+          const client = new sparqlClient(app.endpoint);
+          return client.query(sparql)
+            .then((res) => {
+              app.frogJson = syntaxHighlight(res);
+              return res;
+            });
+        },
+      }).then((res) => {
+        this.loading = false;
+        app.princeJson = syntaxHighlight(res);
+        setTimeout(() => {
+          const tabs = document.querySelector('.tabs');
+          M.Tabs.init(tabs);
+          const instance = M.Tabs.getInstance(tabs);
+          instance.select('prince');
+        }, 10);
+      }).catch((e) => {
+        app.jsonError = e;
+      });
+    },
+  },
+});
+
+
+function syntaxHighlight(json) {
+  if (typeof json !== 'string') {
+    json = JSON.stringify(json, undefined, 2);
+  }
+  json = json.replace(/&/g, '&amp;').replace(/</g, '&lt;').replace(/>/g, '&gt;');
+  return json.replace(/("(\\u[a-zA-Z0-9]{4}|\\[^u]|[^\\"])*"(\s*:)?|\b(true|false|null)\b|-?\d+(?:\.\d*)?(?:[eE][+\-]?\d+)?)/g, (match) => {
+    let cls = 'number';
+    if (/^"/.test(match)) {
+      if (/:$/.test(match)) {
+        cls = 'key';
+      } else {
+        cls = 'string';
+      }
+    } else if (/true|false/.test(match)) {
+      cls = 'boolean';
+    } else if (/null/.test(match)) {
+      cls = 'null';
+    }
+    return `<span class="${cls}">${match}</span>`;
+  });
+}
