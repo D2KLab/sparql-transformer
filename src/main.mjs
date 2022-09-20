f76943e12497266781c365c1e6c664e9759079cf
Thibault Ehrhart <thibault.ehrhart@gmail.com>
2022-09-20 16:34:27 +0200

    Do not generate empty OPTIONAL statements if where condition is empty

--- src/main.mjs
+++ src/main.mjs
@@ -295,8 +295,10 @@ function manageProtoKey(proto, vars = [], filters = [], wheres = [],
       const [mpkFun, bkReq] = manageProtoKey(v, vars, filters, wheresInternal,
         mainLang, prefix + i, _rootId, values);
       Object.keys(v).forEach(mpkFun);
-      wheresInternal = wheresInternal.join('.\n');
-      wheres.push(bkReq ? wheresInternal : `OPTIONAL { ${wheresInternal}}`);
+      if (wheresInternal.length > 0) {
+        wheresInternal = wheresInternal.join('.\n');
+        wheres.push(bkReq ? wheresInternal : `OPTIONAL { ${wheresInternal}}`);
+      }
       return;
     }
     if (typeof v !== 'string') return;
